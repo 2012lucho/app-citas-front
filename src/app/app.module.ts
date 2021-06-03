8cfb7948b40e64bd09c4b103da782513048face9
BuildTools <lucho.2012.tandil@gmail.com>
2021-06-03 18:17:28 -0300

    Solucion a falta de importación de modulo, que saltaba al hacer build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MessageComponent } from './components/message/message.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    MessageComponent
+  ],
   entryComponents: [],
   imports: [
     BrowserModule,
