5e8b1be0637482638f9cc17ff6d739846e9de0d5
BuildTools <lucho.2012.tandil@gmail.com>
2021-06-18 10:02:03 -0300

    se hace pétición de obtención de perfil

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute }    from '@angular/router';
 
-import { AuthService }     from '../../services/auth/auth.service';
-import { ProfileService }  from '../../services/profile.service';
+import { AuthService }       from '../../services/auth/auth.service';
+import { ProfileService }    from '../../services/profile.service';
+import { AppUIUtilsService } from '../../services/app.ui.utils.service';
 
 @Component({
   selector: 'app-profile',
@@ -17,12 +19,41 @@ export class ProfilePage {
     ayuda:          { expanded:false }
   };
 
+  private getProfile:any = null;
+  private getProfileError:any = null;
+
   constructor(
-    private authService:    AuthService,
-    private profileService: ProfileService
+    private authService:       AuthService,
+    private profileService:    ProfileService,
+    private activatedRoute:    ActivatedRoute,
+    private appUIUtilsService: AppUIUtilsService
   ) {}
 
+  private activatedRouteSubject:any = null;
   ngOnInit(): void {
+    this.activatedRouteSubject = this.activatedRoute.params.subscribe((params: any) => {
+        this.profileService.get( this.authService.getUserId() );
+    });
+
+    this.setRequestsSubscriptions();
+  }
+
+  setRequestsSubscriptions(){
+    //GET
+    this.getProfile = this.profileService.getOK.subscribe({  next: ( params: any ) => {
+        this.appUIUtilsService.dismissLoading();
+        console.log(params);
+    } });
+
+    this.getProfileError = this.profileService.getKO.subscribe({  next: ( params: any ) => {
+        this.appUIUtilsService.dismissLoading();
+        this.appUIUtilsService.showMessage('Ocurrió un error, reintente más tarde.');
+    } });
+  }
+
+  unSetRequestsSubscriptions(){
+    this.getProfile.unsubscribe();
+    this.getProfileError.unsubscribe();
   }
 
   toggleExpanded( k:string ){
@@ -31,5 +62,10 @@ export class ProfilePage {
 
   logOut(){
     this.authService.toLogOut();
+  }
+
+  ngOnDestroy(){
+    this.activatedRouteSubject.unsubscribe();
+    this.unSetRequestsSubscriptions();
   }
 }
