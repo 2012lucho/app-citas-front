7b0a9ff1942aa1f153b656853fa0b7b32b7ed33d
BuildTools <lucho.2012.tandil@gmail.com>
2021-07-01 17:22:57 -0300

    Se agrega componente vacío de galería que se usará luego para mostrar las imágenes de perfil en issues subsiguientes

---
+++ src/app/components/gallery/gallery.component.ts
@@ -0,0 +1,15 @@
+import { Component, AfterViewInit, Input, ViewChild,  ElementRef, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss'],
+})
+export class GalleryComponent implements AfterViewInit {
+
+  constructor() {}
+
+  ngAfterViewInit() {
+
+  }
+}
