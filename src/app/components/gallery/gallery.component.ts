2d369c58a1078bb6c830bf6536e4574f17aac009
BuildTools <lucho.2012.tandil@gmail.com>
2021-07-02 14:17:50 -0300

    En componente de galería de imágenes, ahora se muestra el número de la imagen que se ve, por ej img 1/10

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, ViewChild,  ElementRef, Input} from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 import { ImageDataModel } from './image.data.model';
 
@@ -9,14 +10,22 @@ import { ImageDataModel } from './image.data.model';
 })
 export class GalleryComponent implements OnInit {
   @Input() imageData: Array<ImageDataModel> = [];
+  @ViewChild('slides', {read: ElementRef, static:false}) slides: IonSlides;
 
   public slideOpts:any = {
     initialSlide: 1,
     speed: 400
   };
+  public actualIndex:number = 0;
 
   constructor() {}
 
   ngOnInit(): void {
+  }
+
+  onSlideChanged( slide: IonSlides ){
+    slide.getActiveIndex().then((index: number) => {
+      this.actualIndex = index +1;
+    });
   }
 }
