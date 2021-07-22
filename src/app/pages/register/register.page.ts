9b2993d505444fef0cec2a0159dfc7aafe8a9b58
melany.pedemonte <melany.pedemonte@coodesoft.com>
2021-07-22 16:41:24 -0300

    se modifica la vista y se da funcionalidad a el componente de registro

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { Register } from 'src/app/models/register';
+import { AppUIUtilsService } from 'src/app/services/app.ui.utils.service';
+import { Usuario } from 'src/app/models/usuario';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Login } from 'src/app/models/login';
 
 @Component({
   selector: 'app-register',
@@ -7,13 +15,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  public register = new Register();
+  public model = new Usuario();
+  public login = new Login();
+
+  public registerForm: FormGroup;
+
+  private PostOK:any  = null;
+  private PostError:any  = null;
+
+  constructor(
+    private router: Router,
+    private auth:   AuthService,
+    private user: UsuarioService,
+    public  gral:   AppUIUtilsService,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
   next(){
-    console.log("registro");
-  }
+    if(this.registerForm.valid){
+      this.model.username = this.register.username;
+      this.model.email = this.register.email;
+      this.model.password = this.register.password;
+      this.auth.register(this.model);
+
+      this.login.username = this.model.username;
+      this.login.password = this.model.password;
+
+      this.PostOK = this.auth.registerOK.subscribe({  next: ( params: any ) => {
+      if(params["status"]){
+        this.gral.dismissLoading();
+        this.gral.showMessage('Datos actualizados');
+        this.auth.login(this.login);
+      }else{
+        this.gral.dismissLoading();
+        this.gral.showMessage('Ocurrió un error, reintente más tarde.');
+      }
+      } });
+
+      this.PostError = this.auth.registerError.subscribe({  next: ( params: any ) => {
+      this.gral.dismissLoading();
+      this.gral.showMessage('Ocurrió un error, reintente más tarde.');
+      } });
+    
+    }
+ }
 
 }
