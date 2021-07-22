8f05783c105640fe3ed35d6725292ea9dace561d
melany.pedemonte <melany.pedemonte@coodesoft.com>
2021-07-22 16:39:48 -0300

    se agrega componente para la vista de email confirmado

---
+++ src/app/pages/register/confirm-email/confirm-email.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ConfirmEmailComponent } from './confirm-email.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ConfirmEmailComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ConfirmEmailComponent]
+})
+export class ConfirmEmailModule {}
